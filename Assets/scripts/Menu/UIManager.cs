acad2df97ad296ebba27cab5b3c85d73e7fc0cf4
Jeroen Monster <jeroenmonst@gmail.com>
2016-09-28 12:40:42 +0200

    added play services

--- Assets/scripts/Menu/UIManager.cs
+++ Assets/scripts/Menu/UIManager.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 
 public class UIManager : MonoBehaviour {
 
+	//public string leaderboard;
 	public static bool gameOver = false;
 	public bool showGUI = false;
+	public string leaderboard;
 	public static GameObject gameovermenu;
-
 	public static Text distance_score;
 
 	void Start () {
+		PlayGamesPlatform.Activate ();
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder ().Build ();
+		PlayGamesPlatform.DebugLogEnabled = true;
+		PlayGamesPlatform.InitializeInstance (config);
+		LogIn ();
 		Time.timeScale = 1;
-	
 		gameovermenu = GameObject.Find ("GameOverMenu");
 
 		if(gameovermenu != null){
@@ -20,10 +28,20 @@ public class UIManager : MonoBehaviour {
 			gameovermenu.SetActive (false);
 		}
 	}
+	public void LogIn(){
+		Social.localUser.Authenticate ((bool success) => {
+			if (success) {
+				Debug.Log =("Login success");
+			} else {
+				Debug.Log = ("Login Failed");
+			}
+		});
+	}
 
 	public void Update(){
 
 	}
+
 
 	public static void setDistanceScore(int distance){
 		distance_score.text = distance + " km"; 
@@ -55,6 +73,12 @@ public class UIManager : MonoBehaviour {
 
 	public static void showGameOver(){
 		gameovermenu.SetActive(true);
+	}
+	public void onLeaderbord(){
+		Social.ShowLeaderboardUI();
+	}
+	public void onAchievement(){
+			Social.ShowAchievementsUI ();
 	}
 }
 
