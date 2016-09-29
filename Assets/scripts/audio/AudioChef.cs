1150e2f2d9eb71badf046d8c2462305f473a2404
leonstel <leonstel5@gmail.com>
2016-09-29 15:52:22 +0200

    yo

---
+++ Assets/scripts/audio/AudioChef.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioChef : MonoBehaviour 
+{
+	//background music
+	public AudioSource backgroundmusic_layer_1;		
+	public AudioClip background_music;
+
+	public AudioSource fx_layer_1;					
+	             
+	public static AudioChef context = null;
+
+	void Awake ()
+	{
+		context = this;
+
+		playBackgroundMusic ();
+	}
+
+	void Update(){
+		//if(Game.GetInstance().currentStage == Game.Stage.GameOver){
+			//fx_layer_1.Stop ();
+		//}
+	}
+
+	void playBackgroundMusic(){
+		backgroundmusic_layer_1.clip = background_music;
+		backgroundmusic_layer_1.Play ();
+	}
+
+	public static AudioChef getInstance(){
+		return context;
+	}
+}
--- Assets/scripts/orbit_group/OrbitPlanet.cs
+++ Assets/scripts/orbit_group/OrbitPlanet.cs
@@ -2,15 +2,24 @@
 using System.Collections;
 
 public class OrbitPlanet : MonoBehaviour {
+	public Texture2D[] planetTextures;
+
+	private SpriteRenderer sr;
 
 	// Use this for initialization
 	void Start () {
-		float scale = .5f;
+		sr = GetComponent<SpriteRenderer> ();
+
+		Texture2D planetTexture = planetTextures [Random.Range (0, planetTextures.Length)];
+
+		sr.sprite = Sprite.Create(planetTexture,new Rect(0, 0, planetTexture.width, planetTexture.height),new Vector2(0.5f,0.5f));
+
+		float scale = .2f;
 		transform.localScale = new Vector3 (scale,scale,0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
