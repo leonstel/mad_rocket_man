a88bbf579598a1ca90e4d06214ec594d1b2aa452
Jeroen Monster <jeroenmonst@gmail.com>
2016-10-03 11:27:10 +0200

    offlinedatahandler

---
+++ Assets/scripts/Offlinedatahandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Offlinedatahandler : MonoBehaviour {
+	public static int oldScore;
+	public static int newScore;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public static void UpdateHighScore(int newScore){
+		if(PlayerPrefs.HasKey("HScore")){
+			if(PlayerPrefs.GetInt("HScore")<newScore){ 
+				// new score is higher than the stored score
+				oldScore = PlayerPrefs.GetInt("HScore");
+				PlayerPrefs.SetInt("HScore",newScore);
+				newScore = oldScore;
+			}
+		}else{
+			PlayerPrefs.SetInt("HScore",newScore);
+			newScore = 0;
+		}
+	}
+}
